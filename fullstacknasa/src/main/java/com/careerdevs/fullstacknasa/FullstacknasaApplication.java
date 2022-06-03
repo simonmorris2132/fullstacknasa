7c484b928df3ed5c80ecdfba82ef41a6141ea2ae
Simon Morris <simonjmorris213@gmail.com>
2022-06-02 19:24:04 -0500

    first commit

---
+++ fullstacknasa/src/main/java/com/careerdevs/fullstacknasa/FullstacknasaApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.fullstacknasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstacknasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstacknasaApplication.class, args);
+	}
+
+}
